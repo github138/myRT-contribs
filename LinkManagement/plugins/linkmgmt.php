26dc4090cfe56b0ac066be651cb9326a3a2aca24
github138 <m.ehinger@ltur.de>
2015-06-19 11:41:40 +0200

    add ip addresses

--- LinkManagement/plugins/linkmgmt.php
+++ LinkManagement/plugins/linkmgmt.php
@@ -1057,6 +1057,7 @@ class linkmgmt_gvmap {
 				return;
 		}
 
+		$object = NULL;
 		if($object_id !== NULL) {
 			if(
 				!isset($gv->graph['clusters'][$cluster_id]) &&
@@ -1064,6 +1065,15 @@ class linkmgmt_gvmap {
 			) {
 
 				$object = spotEntity ('object', $object_id);
+
+				// ip addresses
+				amplifyCell($object);
+				$object['portip'] = array();
+				foreach($object['ipv4'] as $ipv4)
+				{
+					$object['portip'][$ipv4['osif']] = $ipv4['addrinfo']['ip'];
+				}
+
 			//	$object['attr'] = getAttrValues($object_id);
 
 				$clusterattr = array();
@@ -1139,6 +1149,11 @@ class linkmgmt_gvmap {
 					$nodelabel .= "<BR/><FONT POINT-SIZE=\"8\">${port['iif_name']}</FONT>";
 
 				$nodelabel .= "<BR/><FONT POINT-SIZE=\"8\">${port['oif_name']}</FONT>";
+
+				// add ip address
+				if($object)
+					if(isset($object['portip'][$port['name']]))
+						$nodelabel .= "<BR/><FONT POINT-SIZE=\"8\">".$object['portip'][$port['name']]."</FONT>";
 
 				$nodeattr = array(
 							'label' => $nodelabel,
